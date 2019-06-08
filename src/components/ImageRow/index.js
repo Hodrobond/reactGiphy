98322d96d4a757e9e49444b6448705edbef90d9e
Adam Kazberuk <hodrobond@gmail.com>
2019-06-08 11:42:12 -0700

    It’s rendering…

--- src/components/ImageRow/index.js
+++ src/components/ImageRow/index.js
@@ -4,15 +4,15 @@ const ImageRow = ({ images }) => {
   console.log('IMAGES');
   console.log(images);
   console.log('IN IMAGE ROW');
-  if (!images || !images.images) return null;
+  if (!images) return null;
   console.log('actually rendering');
   return (
   <div className="image-row">
-    {images.images.map(image => (
+    {images.map(image => (
       <img
-        src={imageObj.images.fixed_width.url}
-        width={imageObj.images.fixed_width.width}
-        height={imageObj.images.fixed_width.height}
+        src={image.images.fixed_width.url}
+        width={image.images.fixed_width.width}
+        height={image.images.fixed_width.height}
       />
     ))}
   </div>
